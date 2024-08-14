1904bd4ac84e3408aa35629caf221ca1540f6f45
Sagiv Eliya <u9220921@bsmch.net>
2024-08-15 00:45:54 +0300

    additions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,52 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { MediaContent } from '../media/media.types';
+import { MediaContent, medias } from '../media/media.types';
 import { Item } from '../interactive/interactive.type';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeOut', [
+      state('visible', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('visible => hidden', animate('1s ease-out')),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
-  points: MediaContent[] = [
-    {
-      year: 1964,
-      title: '!חתונה',
-      imageUrl: '',
-      description: 'תחילת הדרך, כמה אושר וחוויות מחכות... !היום המאושר בחייכם',
-    },
-    {
-      year: 2014,
-      title: '!50 שנות נישואין',
-      imageUrl: '',
-      description: 'תחילת הדרך, כמה אושר וחוויות מחכות...  !היום המאושר בחייכם',
-    },
-    {
-      year: 2024,
-      title: '!שנות נישואין 60',
-      imageUrl: '',
-      description: 'תחילת הדרך, כמה אושר וחוויות מחכות... !היום המאושר בחייכם',
-    },
-  ];
+  points: MediaContent[] = medias;
 
-  selectedMedia: MediaContent;
-  mediaTop: string;
   family: Item = {
     name: 'family',
     text: 'משפוחה',
   };
+
   ouzo: Item = {
     name: 'ouzo',
     text: '?חגיגות אה',
   };
 
+  selectedMedia: MediaContent = null;
+  mediaTop: string;
+  showStartScreen = true;
+  splashState = 'visible';
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  openMedia(point: any, event: MouseEvent): void {
+  openMedia(point: MediaContent, event: MouseEvent): void {
     this.selectedMedia = point;
     const rect = (event.target as HTMLElement).getBoundingClientRect();
     this.mediaTop = `${rect.top + window.scrollY + rect.height + 10}px`;
   }
+
+  hideStartScreen = () => {
+    this.splashState = 'hidden';
+
+    setTimeout(() => {
+      this.showStartScreen = false;
+    }, 1000);
+  };
 }
